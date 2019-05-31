244bf71756343a5b19619e125b1b1508f1ac90b0
tonyiovino <antonioiovino2004tony@gmail.com>
2019-05-31 22:03:36 +0000

    Endurance2

---
+++ endurance2.py
@@ -0,0 +1,10 @@
+print("Inserisci la quantità del carburante: ")
+quantita = input()
+print("Inerisci il consumo orario del carburante: ")
+consumo = input()
+
+if quantita < 1:
+	print(quantita)
+#devo ancorab finire.
+#devo salvare per domani.
+
