ebac5965a7add52dad35de2f1cb8c5c13bb48905
tonyiovino <antonioiovino2004tony@gmail.com>
2019-06-10 15:40:05 +0000

    Endurance

--- endurance2.py
+++ endurance2.py
@@ -1,10 +1,25 @@
-print("Inserisci la quantità del carburante: ")
-quantita = input()
-print("Inerisci il consumo orario del carburante: ")
-consumo = input()
+quantita_str = input("Inserite la quantità in galloni del carburante: ")
+quantita = float(quantita_str)
 
-if quantita < 1:
-	print(quantita)
-#devo ancorab finire.
-#devo salvare per domani.
+consumo_orario_str = input("Inserite il consumo orario: ")
+consumo_orario = float(consumo_orario_str)
+
+if quantita < 0:
+	print("La quantità del carburante deve essere maggiore di 0.")
+elif consumo_orario < 0:
+	print("Il consumo deve essere maggiore di 0.")
+else:
+	tempo = quantita / consumo_orario
+
+	ore = int(tempo)
+
+	tempo = tempo - ore
+	tempo = tempo * 60
+	minuti = int(tempo)
+
+	tempo = tempo - minuti
+	tempo = tempo * 60
+	secondi = int(tempo)
+
+	print("Tempo in volo: ", ore, "h", minuti, "m", secondi, "s")
 
